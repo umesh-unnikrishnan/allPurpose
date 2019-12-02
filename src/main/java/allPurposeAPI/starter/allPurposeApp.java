92d32edb2b3a982ffdbd52b812391c6a7bb94b46
umesh unnikrishnan <umeshanfloyd@gmail.com>
2019-12-03 00:40:44 +0530

    Added basic code

---
+++ src/main/java/allPurposeAPI/starter/allPurposeApp.java
@@ -0,0 +1,14 @@
+package allPurposeAPI.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class allPurposeApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(allPurposeApp.class, args);
+
+	}
+
+}
